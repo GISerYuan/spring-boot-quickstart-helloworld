6ea42681dd75a06a6aada1ad077bee6039ee41a4
GISer_Yuan <liqy0427@163.com>
2019-09-26 09:43:43 +0800

    spring boot demo project

---
+++ src/main/java/com/chinadci/SpringBootQuickstartHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.chinadci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuickstartHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickstartHelloworldApplication.class, args);
+	}
+
+}
